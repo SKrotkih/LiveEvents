0bf1b3f58ca162fd24c76851d5e6c97fdb3f7d25
Serhii Krotkih <sergey.krotkih@gmail.com>
2022-10-10 19:14:03 +0300

    Implement Video list screen

--- Scenes/VideoList/View/VideoListView.swift
+++ Scenes/VideoList/View/VideoListView.swift
@@ -22,41 +22,47 @@ protocol VideoListViewModelInterface: ObservableObject {
 struct VideoListView<ViewModel>: View where ViewModel: VideoListViewModelInterface {
     @EnvironmentObject var store: AuthReduxStore
     @ObservedObject var viewModel: ViewModel
+    @Environment(\.presentationMode) var presentationMode
 
     var body: some View {
-        VStack {
-            VideoList(sections: viewModel.sections)
-            ErrorMessage(text: viewModel.errorMessage)
-        }
-        .padding(.top, 30.0)
-        .padding(.bottom, 30.0)
-        .onAppear {
-            viewModel.loadData()
-        }
-    }
-}
-
-struct VideoList: View {
-    let sections: [VideoListSection]
-    init(sections: [VideoListSection]) {
-        self.sections = sections
-    }
-
-    var body: some View {
-        List {
-            ForEach(sections, id: \.self) { section in
-                Text(section.sectionName)
-                    .foregroundColor(.white)
-                ForEach(section.rows, id: \.self) { item in
-                    HStack {
-                        Text(item.title)
-                            .foregroundColor(.green)
-                        Spacer(minLength: 5.0)
-                        Text("\(item.publishedAt)")
-                            .foregroundColor(.green)
-                    }
+        NavigationView {
+            List {
+                ForEach(viewModel.sections, id: \.self) { section in
+                    Section(header: Text(section.sectionName)
+                        .font(.title)
+                        .foregroundColor(.white)) {
+                            ForEach(section.rows, id: \.self) { item in
+                                NavigationLink(destination: VideoPlayerScene(item: item)) {
+                                    HStack {
+                                        Text(item.title)
+                                            .foregroundColor(.green)
+                                        Spacer(minLength: 5.0)
+                                        Text("\(item.publishedAt)")
+                                            .foregroundColor(.green)
+                                    }
+                                }
+                            }
+                        }
                 }
             }
+            .listStyle(GroupedListStyle())
+            .navigationBarTitle(Text("My video list").font(.title), displayMode: .inline)
+            .edgesIgnoringSafeArea(.bottom)
+            .navigationBarBackButtonHidden(true)
+            .navigationBarItems(leading:
+                                    Button(action: {
+                self.presentationMode.wrappedValue.dismiss()
+            }) {
+                HStack {
+                    Image(systemName: "arrow.left.circle")
+                    Text("Back")
+                        .foregroundColor(.white)
+                }
+            })
+            //                ErrorMessage(text: viewModel.errorMessage)
+        }
+        .onAppear {
+            viewModel.loadData()
         }
     }
 }
@@ -69,6 +75,18 @@ struct ErrorMessage: View {
 
     var body: some View {
         Text(text)
+            .foregroundColor(.red)
+    }
+}
+
+struct VideoPlayerScene: View {
+    let item: VideoListRow
+    init(item: VideoListRow) {
+        self.item = item
+    }
+
+    var body: some View {
+        Text(item.title)
             .foregroundColor(.red)
     }
 }
